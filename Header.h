3e33304f06255f26a449035c0fe24be452c92bef
igrutk <106422717+igrutk@users.noreply.github.com>
2023-01-09 21:37:53 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void swap(T* p_a, T* p_b) {
+	T* p_c = new T(*p_a);
+	*p_a = *p_b;
+	*p_b = *p_c;
+	delete p_c;
+}
---
+++ task1.cpp
@@ -0,0 +1,12 @@
+﻿//задание 1
+#include "Header.h"
+using namespace std;
+
+int main() {
+	cout << "Task 1" << endl;
+	int x1 = 4;
+	int x2 = 3;
+	cout << "Before swap ( " << x1 << " ; " << x2 << " )" << endl;
+	swap(&x1, &x2);
+	cout << "After swap ( " << x1 << " ; " << x2 << " )" << endl;
+}

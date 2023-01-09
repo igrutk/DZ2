b1dfe99dae0d14f82a186de3f567789a1237843a
igrutk <106422717+igrutk@users.noreply.github.com>
2023-01-09 21:54:50 +0300

    Add files via upload

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 3;
+	int* a = new int[n] { 1, 2, 3 };
+	for (int i = 0; i < n; ++i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	int* pa = a;
+	int* pla = a + n - 1;
+	swap(pa, pla);
+	cout << "Changed array: ";
+	for (int i = 0; i < n; ++i) {
+		cout << a[i] << " ";
+	}
+	std::cout << std::endl;
+}

e61d342b676e57c00d68474f110d91492be4914b
igrutk <106422717+igrutk@users.noreply.github.com>
2023-01-09 23:13:29 +0300

    Update and rename task6.cpp to task6-7.cpp

--- task6-7.cpp
+++ task6-7.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main{
+	//задание 6
 	int n_2 = 14;
 	double* array = new double[n_2] { 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.1, 2.2, 2.3, 2.4, 2.5 };
 	array_change_halfs(array, n_2);
@@ -11,3 +12,9 @@ int main{
 		cout << array[i] << " ";
 	}
 	cout << endl;
+	
+	//задание 7
+	int n7 = 14;
+	double* arr7 = new double[n7] { 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.1, 2.2, 2.3, 2.4, 2.5};
+	cout << "Amount of local minimums = " << local_min(arr7, n7) << endl;
+}
